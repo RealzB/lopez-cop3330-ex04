c5da0e09ff2414c549e3b98b4d15e812af1620af
Brandon <nickelboarder123.bl@gmail.com>
2021-09-13 22:12:45 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Brandon Lopez
+ */
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a noun:");
+        String noun = sc.nextLine();
+        System.out.println("Enter a verb:");
+        String verb = sc.nextLine();
+        System.out.println("Enter an adjective:");
+        String adjective = sc.nextLine();
+        System.out.println("Enter an adverb:");
+        String adverb = sc.nextLine();
+
+        String madLib = String.format("Do you %s your %s %s %s? That's hilarious!", verb, adjective, noun, adverb);
+
+        System.out.println(madLib);
+
+    }
+}
